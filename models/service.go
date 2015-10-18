3f273fe83c4803f6a24384a9d71df79848accc49
Tony Ivanov <telamohn@gmail.com>
2015-10-18 12:05:36 +0200

    Refactored Service to use ContainerID for latching

--- models/service.go
+++ models/service.go
@@ -17,14 +17,13 @@ const (
 )
 
 type Version struct {
-	Brief     *docker.APIContainers
 	Container *docker.Container
 	Image     *docker.Image
 }
 type Service struct {
-	Image   string
+	Name    string
 	State   string
-	Current Version
+	Current *Version
 }
 
 var Client *docker.Client
@@ -38,7 +37,7 @@ func init() {
 }
 func (t *Service) ensureState(state string) {
 	if t.State != state {
-		panic(fmt.Sprintf("Expected state `%s` to be `%s`", t.State, state))
+		panic(fmt.Sprintf("State `%s` has to be `%s`", t.State, state))
 	}
 }
 
@@ -64,49 +63,73 @@ func (t *Service) transition(state string) (string, error) {
 	return t.State, nil
 }
 
-func CreateService(image string) (*Service, error) {
-	service := Service{State: Initializing, Image: image}
-	return &service, nil
+func CreateService(name string) *Service {
+	service := Service{State: Initializing, Name: name}
+	return &service
+}
+
+/**
+* This method is mainly used by testsuite, won't have
+* real world uses.
+ */
+func CreateServiceFromImage(image string) (*Service, error) {
+	service := CreateService("Service: " + image)
+
+	containers, err := Client.ListContainers(docker.ListContainersOptions{})
+	if err != nil {
+		return nil, err
+	}
+	var cid string = ""
+	var brief *docker.APIContainers
+	for _, container := range containers {
+		if container.Image == image {
+			cid = container.ID
+			brief = &container
+			break
+		}
+	}
+	if cid == "" {
+		service.transition(Borked)
+		return service, errors.New(fmt.Sprintf("Container `%s` is not running", image))
+	}
+	err = service.Latch(cid)
+	if err != nil {
+		service.transition(Borked)
+		return service, err
+	}
+	if false {
+		Println("---------- Brief ---------")
+		Println(brief)
+		Println("---------- Container ---------")
+		Println(service.Current.Container)
+		Println("---------- Image ---------")
+		Println(service.Current.Image)
+	}
+	return service, nil
 }
 
 /* Latches onto a runing container matched by container.Image
 * and populates the container.Current with gathered data
  */
-func (t *Service) Latch() error {
+func (t *Service) Latch(cid string) error {
 	t.ensureState(Initializing)
-	containers, err := Client.ListContainers(docker.ListContainersOptions{})
-	if err != nil {
-		return err
-	}
-	t.Current = Version{}
-	for _, container := range containers {
-		if container.Image == t.Image {
-			t.Current.Brief = &container
-			break
-		}
-	}
-	if t.Current.Brief == nil {
-		return errors.New(fmt.Sprintf("Container `%s` is not running", t.Image))
-	}
-	Println("---------- Brief ---------")
-	Println(t.Current.Brief)
-	t.Current.Container, err = Client.InspectContainer(t.Current.Brief.ID)
+	var err error
+	ver := Version{}
+	ver.Container, err = Client.InspectContainer(cid)
 	if err != nil {
 		return err
 	}
 
-	Println("---------- Container ---------")
-	Println(t.Current.Container)
-	t.Current.Image, err = Client.InspectImage(t.Current.Container.Image)
+	ver.Image, err = Client.InspectImage(ver.Container.Image)
 	if err != nil {
 		return err
 	}
 
-	if t.Current.Image == nil {
-		return errors.New(fmt.Sprintf("Image `%s` could not be resolved?!?", t.Image))
+	if ver.Image == nil {
+		return errors.New(fmt.Sprintf("Image `%s` could not be resolved?!?", ver.Container.Image))
 	}
-	Println("---------- Image ---------")
-	Println(t.Current.Image)
+	t.Current = &ver
+
 	// Alright we have all the info we need for now, transition to running
 	t.transition(Running)
 	return nil
--- service_test.go
+++ service_test.go
@@ -13,26 +13,27 @@ import (
 var _ = Describe("Service", func() {
 	Context("The service state-machine", func() {
 		It("should start in state `init`", func() {
-			service, err := CreateService("_/debian")
-			Expect(err).NotTo(HaveOccurred())
+			service := CreateService("magical-dishwasher")
 			Expect(service).ToNot(BeNil())
+			Expect(service.Current).To(BeNil())
 			Expect(service.State).To(Equal(Initializing))
 		})
 	})
 	Context("Latching a Service onto running Container", func() {
 
-		It("should find ImageID,ContainerID and Tag", func() {
-			service, err := CreateService("busybox")
-			Expect(err).NotTo(HaveOccurred())
-			err = service.Latch()
+		It("Should find and set Container and Image", func() {
+			service, err := CreateServiceFromImage("busybox")
 			Expect(err).NotTo(HaveOccurred())
 			Expect(service.Current).ToNot(BeNil())
 			Expect(service.Current.Container).ToNot(BeNil())
+			Expect(service.Current.Image).ToNot(BeNil())
 			Expect(service.State).To(Equal(Running))
-
+		})
+		It("Should go to into borked state if container isn't running", func() {
+			service, err := CreateServiceFromImage("")
+			Expect(err).To(HaveOccurred())
+			Expect(service.State).To(Equal(Borked))
 		})
 	})
 
-	Context("Docker daemon communication", func() {
-	})
 })
