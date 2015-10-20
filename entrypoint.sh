e1f18e69236253ed4d3af2b397a510b769664ca0
Tony Ivanov <telamohn@gmail.com>
2015-10-20 08:28:52 +0200

    Refactored inside container testing
    Moved contents of DockerTest into an interactive 'test' switch
    into the entrypoint.sh script.
    So running the container with command 'test' will download and install
    testsuite and go.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,37 @@ set -e
 echo "" > /app/vhosts.conf
 # Let nginx fork off to background.
 exec nginx & 
-exec "$@"
+
+if [ "$1" = "test" ]; then
+	export GOLANG_VERSION="1.5.1"
+	export GOLANG_DOWNLOAD_URL="https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz"
+	export GOLANG_DOWNLOAD_SHA1="46eecd290d8803887dec718c691cc243f2175fe0"
+	export GOPATH="/go"
+	export PATH="$GOPATH/bin:/usr/local/go/bin:$PATH"
+	export PROJECT_PATH="/go/src/github.com/telamon/wharfmaster"
+
+	if [ ! -f "/app/wharfmaster.go" ]; then
+		echo "Test command is intended to be used with the source-code volumed on /app -v $PWD:/app"
+		exit 1
+	fi
+	# Initialize destenvironment
+	if [ ! -L $PROJECT_PATH ]; then
+		if [ ! -f "golang.tar.gz" ]; then
+			echo "Setting up test-environment" \
+			&& curl -fL "$GOLANG_DOWNLOAD_URL" -o golang.tar.gz \
+			&& echo "$GOLANG_DOWNLOAD_SHA1  golang.tar.gz" | sha1sum -c - 
+		fi
+		if [ ! -d "$GOPATH" ]; then
+			tar -C /usr/local -xzf golang.tar.gz \
+			&& mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 777 "$GOPATH" \
+			&& apt-get install -y git \
+			&& go get github.com/onsi/ginkgo/ginkgo \
+			&& go get github.com/onsi/gomega 
+		fi
+		mkdir -p $PROJECT_PATH  && rm -r $PROJECT_PATH \
+		&& ln -sf /app $PROJECT_PATH 
+	fi
+	cd $PROJECT_PATH && go get && ginkgo watch
+else
+	exec "$@"
+fi
--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # this file is just a memo,
 # committed for later
-docker build -t wharfmaster_test -f DockerfileTest .
-docker run --rm -it  -v $PWD:/go/src/github.com/telamon/wharfmaster -v /var/run/docker.sock:/tmp/docker.sock wharfmaster_test bash
+docker run --name=wharftest -it  -v $PWD:/app -v /var/run/docker.sock:/tmp/docker.sock wharfmaster_app test
